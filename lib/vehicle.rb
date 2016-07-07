74b65dffe24d7fcacc2b42ca575bb5d3f7689d04
Noah Ramey <noahramey1997@gmail.com>
2016-07-07 09:09:47 -0700

    working

---
+++ lib/vehicle.rb
@@ -0,0 +1,52 @@
+class Vehicle
+  attr_reader(:make, :model, :year, :color, :engine_size, :number_of_doors)
+  @@vehicles = []
+
+  define_method(:initialize) do |attributes|
+    @make = attributes.fetch(:make)
+    @model = attributes.fetch(:model)
+    @year = attributes.fetch(:year)
+    @color = attributes.fetch(:color)
+    @engine_size= attributes.fetch(:engine_size)
+    @number_of_doors = attributes.fetch(:number_of_doors)
+    @id = @@vehicles.length().+(1)
+  end
+
+  define_method(:id) do
+    @id
+  end
+
+  define_singleton_method(:all) do
+    @@vehicles
+  end
+
+  define_method(:save) do
+    @@vehicles.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@vehicles = []
+  end
+
+  define_method(:age) do
+    current_year = Time.new().year()
+    current_year.-(@year)
+  end
+
+  define_method(:worth_buying?) do
+    american_cars = ['Chrysler', 'Ford', 'GM']
+    american_cars.include?(@make).&(self.age().<=15)
+  end
+
+
+
+  define_singleton_method(:find) do |identification|
+    found_vehicle = nil
+    @@vehicles.each() do |vehicle|
+      if vehicle.id().eql?(identification.to_i())
+        found_vehicle = vehicle
+      end
+    end
+    found_vehicle
+  end
+end
---
+++ spec/vehicle_spec.rb
@@ -0,0 +1,95 @@
+require('rspec')
+require('vehicle')
+
+describe('Vehicle') do
+  before() do
+    Vehicle.clear()
+  end
+
+  describe('#make') do
+    it('returns the make of the vehicle') do
+      test_vehicle = Vehicle.new({:make => 'Honda', :model => 'Civic', :year => 2001, :color => 'blue', :engine_size => '4L', :number_of_doors =>
+      '4'})
+      expect(test_vehicle.make()).to(eq('Honda'))
+    end
+  end
+
+  describe('#model') do
+    it('returns the model of the vehicle') do
+      test_vehicle = Vehicle.new({:make => 'Honda', :model => 'Civic', :year => 2001, :color => 'blue', :engine_size => '4L', :number_of_doors =>
+      '4'})
+      expect(test_vehicle.model()).to(eq('Civic'))
+    end
+  end
+
+  describe('#year') do
+    it('returns the year of the vehicle') do
+      test_vehicle = Vehicle.new({:make => 'Honda', :model => 'Civic', :year => 2001, :color => 'blue', :engine_size => '4L', :number_of_doors =>
+      '4'})
+      expect(test_vehicle.year()).to(eq(2001))
+    end
+  end
+
+  describe('.all') do
+    it('is empty at first') do
+      expect(Vehicle.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('adds a vehicle to the array of saved vehicles') do
+      test_vehicle = Vehicle.new({:make => 'Honda', :model => 'Civic', :year => 2001, :color => 'blue', :engine_size => '4L', :number_of_doors =>
+      '4'})
+      test_vehicle.save()
+      expect(Vehicle.all()).to(eq([test_vehicle]))
+    end
+  end
+
+  describe('.clear') do
+    it('empties out all of the saved vehicles') do
+      test_vehicle = Vehicle.new({:make => 'Honda', :model => 'Civic', :year => 2001, :color => 'blue', :engine_size => '4L', :number_of_doors =>
+      '4'})
+      test_vehicle.save()
+      Vehicle.clear()
+      expect(Vehicle.all()).to(eq([]))
+    end
+  end
+
+  describe('#age') do
+    it('returns the vehicles age') do
+      test_vehicle = Vehicle.new({:make => 'Honda', :model => 'Civic', :year => 2001, :color => 'blue', :engine_size => '4L', :number_of_doors =>
+      '4'})
+      expect(test_vehicle.age()).to(eq(15))
+    end
+  end
+
+  describe('#worth_buying?') do
+    it('returns false if the car is not American made and less than 15 years old') do
+      test_vehicle = Vehicle.new({:make => 'Honda', :model => 'Civic', :year => 2001, :color => 'blue', :engine_size => '4L', :number_of_doors =>
+      '4'})
+      expect(test_vehicle.worth_buying?()).to(eq(false))
+    end
+  end
+
+
+  describe("#id") do
+    it("returns the id of the vehicle") do
+      test_vehicle = Vehicle.new({:make => 'Honda', :model => 'Civic', :year => 2001, :color => 'blue', :engine_size => '4L', :number_of_doors =>
+      '4'})
+      test_vehicle.save()
+      expect(test_vehicle.id()).to(eq(1))
+    end
+  end
+
+  describe('.find') do
+    it('returns a vehicle by its id number') do
+      test_vehicle = Vehicle.new({:make => 'Honda', :model => 'Civic', :year => 2001, :color => 'blue', :engine_size => '4L', :number_of_doors =>
+      '4'})
+      test_vehicle.save()
+      test_vehicle2 = Vehicle.new({:make => 'Toyota', :model => 'Corolla', :year => 1999, :color => 'red', :engine_size => '4L', :number_of_doors =>
+      '4'})
+      test_vehicle2.save()
+      expect(Vehicle.find(test_vehicle.id())).to(eq(test_vehicle))
+    end
+  end
+end
